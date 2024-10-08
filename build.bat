580463d5619cb9b6f8b5bbc8c81ac96b8ac0131e
Akadeax <patrickqwertz@gmail.com>
2024-10-08 13:38:15 +0200

    Initial commit

---
+++ build.bat
@@ -0,0 +1,13 @@
+SET WORKSPACE_DIR=%~dp0
+
+SET BUILD_DIR=build
+SET SRC_DIR=src
+
+SET MAIN=main
+SET CFG=game.cfg
+SET CHR=game.chr
+SET OUT_NAME=MyGame
+
+@call scripts/cleanup.bat
+@call scripts/assemble.bat
+@call scripts/link.bat
